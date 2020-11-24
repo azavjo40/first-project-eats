30313a0cb104cdd225712436fb5abd42d04bc057
azavjo40 <pabek92@gmail.com>
2020-11-24 12:14:36 +0100

    add navbar and customizatoin

---
+++ eats-app/src/App.js
@@ -0,0 +1,16 @@
+import Eastnas from './components/Navbar'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button  } from 'react-bootstrap'
+function App() {
+  return (
+    <>
+ <Eastnas />
+    <div className="container"> 
+
+    </div>
+    </>
+  )
+}
+
+export default App
