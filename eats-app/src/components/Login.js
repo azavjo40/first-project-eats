361e57cfc3d794fac70e74041f4b420575863034
azavjo40 <pabek92@gmail.com>
2020-11-30 15:01:21 +0100

    add contacts

--- eats-app/src/components/Login.js
+++ eats-app/src/components/Login.js
@@ -1,6 +1,38 @@
-import React from 'react'
-import {Container,Form ,Button } from 'react-bootstrap'
+import {useState, useEffect} from 'react'
+import {Form ,Button,Container } from 'react-bootstrap'
+import { useHttp } from '../hooks/http.hook'
+import { useMesaage } from '../hooks/message.hook'
+
 function Login() {
+const message = useMesaage()
+
+const [form, setForm] = useState({ email: '', password: ''})
+
+
+const changehandler = (event)=>{
+  setForm({...form, [event.target.name]: event.target.value })
+
+} 
+
+//const [mess, setMess] = useState(null)
+const { request, loading, error, clearError} = useHttp()
+
+  //  следить за ошибка и отправляем пз
+  useEffect(()=>{
+    message(error)
+    clearError()
+    },[error,message, clearError])
+
+  // fn login
+const loginHandler = async () =>{
+  try{
+  const data = await request('/api/auth/login','POST', {...form})
+    message(data.name)
+  
+   
+  }catch(e){}
+  }
+
   return (
     <>
 <Container fluid="md" style={{marginTop: "3rem"}} >
@@ -8,7 +40,12 @@ function Login() {
   <Form>
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Login</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control type="email"
+     placeholder="Enter email" 
+     onChange={changehandler }
+     name="email"
+     value={form.email}
+     />
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -16,9 +53,20 @@ function Login() {
 
   <Form.Group controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Control 
+    type="password" 
+    placeholder="Password"
+    onChange={changehandler }
+    name="password"
+     value={form.password}
+    />
   </Form.Group>
-  <Button variant="primary" type="submit">
+  <Button 
+  variant="primary"
+   type="submit"
+   onClick={loginHandler}
+   disabled={loading}
+   >
     Submit
   </Button>
 </Form>
--- eats-app/src/pages/Contact.js
+++ eats-app/src/pages/Contact.js
@@ -1,11 +1,36 @@
 import React from 'react'
-
+import {Form ,Button,Container } from 'react-bootstrap'
 function Contact() {
   return (
+
     <>
-<h1>Hello Contact</h1>
+<Container fluid="md" style={{marginTop: "3rem"}} >
+<Form>
+  <Form.Group controlId="formBasicEmail">
+
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="name" placeholder="Name" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Surname</Form.Label>
+    <Form.Control type="surname" placeholder="Surname" />
+  </Form.Group>
+
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Message</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+
+  <Button variant="primary" type="Send">
+    Submit
+  </Button>
+</Form>
+ 
+</Container>
     </>
+
   )
-}
+  }
 
 export default Contact
